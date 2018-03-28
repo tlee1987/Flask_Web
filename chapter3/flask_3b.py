2338fb65d4dba0096bc11b351f4a5f1b7aaa4c12
lt <litiandlut@163.com>
2018-03-29 00:01:18 +0800

    add sth.

--- chapter3/flask_3b.py
+++ chapter3/flask_3b.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
-# -*- coding=utf-8 -*-
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')

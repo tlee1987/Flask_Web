3e94f7ea6a49edc390be3dea579f7d3220d7de7e
litian <litiandlut@163.com>
2018-04-19 19:27:20 +0800

    add  sth.

---
+++ Flask-Mega/app/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_openid import OpenID
+
+from config import basedir
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from . import views, models
